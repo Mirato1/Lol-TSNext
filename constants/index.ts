1c6ac937cf6a5e13a6b260decb898175a108c2da
Lucas Nahuel Giménez <luquetti.5@hotmail.com>
2023-08-01 17:43:05 -0300

    🎉 Initial Commit: Dependencies & Next 13.4

---
+++ constants/index.ts
@@ -0,0 +1,4 @@
+export const API = process.env.API_URL;
+export const URL_IMG_PATCH = `${process.env.API_URL}${process.env.API_PATCH}/img`;
+export const URL_IMG = `${process.env.API_URL}img`;
+export const URL_DATA = `${process.env.API_URL}${process.env.API_PATCH}/data/en_US/`;
